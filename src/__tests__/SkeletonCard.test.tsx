fbac555df5b929ec576daed3c7dce6cea347f696
Juan Calle <juan.calle2@nordstrom.com>
2025-07-03 07:28:51 -0500

    deleting unnecesesary code

--- src/__tests__/SkeletonCard.test.tsx
+++ src/__tests__/SkeletonCard.test.tsx
@@ -4,9 +4,7 @@ import SkeletonCard from '../components/Skeletons/Card/SkeletonCard';
 describe('SkeletonCard', () => {
   it('renders skeleton elements', () => {
     const { container } = render(<SkeletonCard />);
-    console.log(container)
     const skeletons = container.querySelectorAll('[class*="skeleton"]');
-    console.log(skeletons)
     expect(skeletons.length).toBeGreaterThan(0);
   });
 });
